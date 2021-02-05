d1c0c8260c3d20c88b6e3f7ce3ec756c5a374dae
Hektor-Byfors <Hektor.Byfors@hotmail.com>
2021-02-05 17:06:41 +0100

    burger menu in component done

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,39 @@
 <template>
-  
+  <Menu />
+  <h1 class="flexCenter">Burger<br>Menu!</h1>
 </template>
 
 <script>
-export default {
+  import Menu from './components/Menu.vue'
 
+export default { 
+  components: {
+    Menu
+  }
 }
 </script>
 
 <style>
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: 'Courier New', Courier, monospace;
+  }
 
+  body {
+    background-color: rgb(211, 211, 211);
+  }
+  
+  .flexCenter {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  h1 {
+    font-size: 100px;
+    text-align: center;
+    margin-top: 50px;
+    color: rgb(75, 75, 75);
+  }
 </style>
--- src/components/menu.vue
+++ src/components/menu.vue
@@ -1,13 +1,67 @@
 <template>
-  
+  <div v-show="showMenu" class="burger-menu">
+    <ul>
+      <li>Sample text</li>
+      <li>Sample text</li>
+      <li>Sample text</li>
+      <li>Sample text</li>
+      <li>Sample text</li>
+    </ul>
+  </div>
+  <span v-on:click="toggleShowMenu()" class="material-icons flexCenter">notes</span>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      showMenu: false
+    }
+  },
+  methods: {
+    toggleShowMenu() {
+      this.showMenu = !this.showMenu
+    }
+  }
 }
 </script>
 
 <style>
+.material-icons {
+  font-size: 40px;
+  color: rgb(224, 221, 0);
+  background-color: gray;
+  border-radius: 3px;
+  width: 45px;
+  height: 45px;
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  cursor: pointer;
+}
+
+ul {
+  position: fixed;
+  background-color: gray;
+  right: 20px;
+  top: 20px;
+  list-style-type: none;
+  border-radius: 3px;
+  padding: 50px 20px;
+}
+
+li {
+  color: rgb(224, 221, 0);
+  font-weight: 600;
+  font-size: 22px;
+  margin: 10px;
+  border-bottom: 2px solid gray;
+  cursor: pointer;
+}
+
+li:hover {
+  border-bottom: 2px solid white;
+  transition: 500ms;
+}
 
 </style>
